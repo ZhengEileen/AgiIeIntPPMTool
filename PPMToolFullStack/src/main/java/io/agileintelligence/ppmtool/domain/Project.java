3a8faab5300c988d94a56e0fb76b627dad9b0a9f
Eileen Zheng <fangquan.zheng@outlook.com>
2023-04-26 17:40:45 +0800

    add project validation

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,14 +14,26 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
-    private Date updateed_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date updated_At;
 
     public Project() {
     }
@@ -78,12 +94,12 @@ public class Project {
         this.created_At = created_At;
     }
 
-    public Date getUpdateed_At() {
-        return updateed_At;
+    public Date getUpdated_At() {
+        return updated_At;
     }
 
-    public void setUpdateed_At(Date updateed_At) {
-        this.updateed_At = updateed_At;
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
     }
 
     @PrePersist
@@ -93,7 +109,7 @@ public class Project {
 
     @PreUpdate
     protected void onUpdate(){
-        this.updateed_At = new Date();
+        this.updated_At = new Date();
     }
 
 
