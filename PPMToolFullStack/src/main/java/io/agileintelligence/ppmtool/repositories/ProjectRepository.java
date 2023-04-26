1ace02b65823cb02ba3e894e581ef6319a7fddaf
Eileen Zheng <fangquan.zheng@outlook.com>
2023-04-26 17:20:18 +0800

    create service and controller, add object by postman

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -1,4 +1,15 @@
 package io.agileintelligence.ppmtool.repositories;
 
-public interface ProjectRepository {
+import io.agileintelligence.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+
+
+
 }
+
