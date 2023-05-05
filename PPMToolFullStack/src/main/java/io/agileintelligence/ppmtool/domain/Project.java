17482366d98950b4cc61facef76dd3b211a463f4
Eileen Zheng <fangquan.zheng@outlook.com>
2023-05-05 09:52:19 +0800

    Entity Relationships: Project and Backlog

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,5 +1,6 @@
 package io.agileintelligence.ppmtool.domain;
 
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
@@ -13,28 +14,27 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank(message = "Project name is required")
     private String projectName;
-
-    @NotBlank(message = "Project Identifier is required")
-    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @NotBlank(message ="Project Identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
     @Column(updatable = false, unique = true)
     private String projectIdentifier;
-
     @NotBlank(message = "Project description is required")
     private String description;
-
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
-
     @JsonFormat(pattern = "yyyy-mm-dd")
     @Column(updatable = false)
     private Date created_At;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+    //
+    private Backlog backlog;
 
     public Project() {
     }
@@ -103,6 +103,14 @@ public class Project {
         this.updated_At = updated_At;
     }
 
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
+    }
+
     @PrePersist
     protected void onCreate(){
         this.created_At = new Date();
@@ -113,17 +121,4 @@ public class Project {
         this.updated_At = new Date();
     }
 
-    @Override
-    public String toString() {
-        return "Project{" +
-                "id=" + id +
-                ", projectName='" + projectName + '\'' +
-                ", projectIdentifier='" + projectIdentifier + '\'' +
-                ", description='" + description + '\'' +
-                ", start_date=" + start_date +
-                ", end_date=" + end_date +
-                ", created_At=" + created_At +
-                ", updated_At=" + updated_At +
-                '}';
-    }
 }
